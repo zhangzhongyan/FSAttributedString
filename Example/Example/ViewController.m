9b3549225eb9ee110b7f30eb4a02107b4dc576f6
zhangzhongyan <749994100@qq.com>
2020-05-25 18:51:14 +0800

    添加文件

---
+++ Example/Example/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  Example
+//
+//  Created by 张忠燕 on 2020/5/25.
+//  Copyright © 2020 张忠燕. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <FSAttributedString/FSAttributedString.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
---
+++ Podfile
@@ -0,0 +1,9 @@
+workspace 'FSAttributedString'
+
+platform :ios, '8.0'
+
+project 'Example/Example'
+target 'Example' do
+  pod 'Masonry'
+  pod 'FSAttributedString', :path => './'
+end
